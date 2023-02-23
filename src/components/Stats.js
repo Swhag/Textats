0780a8d914ce6eb9414237b60651dbf0567d01d1
Swhag <khg5293@outlook.com>
2023-02-23 01:01:29 -0500

    file directory structure updated

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,9 +1,6 @@
 import React from 'react';
 import '../styles/Stats.css';
 
-import LetterStats from './LetterStats';
-import WordStats from './WordStats';
-
 function Stats(props) {
   const { letterStats, wordStats } = props;
 
@@ -11,6 +8,48 @@ function Stats(props) {
     <div className='stats-container'>
       <LetterStats letterStats={letterStats} />
       <WordStats wordStats={wordStats} />
+    </div>
+  );
+}
+
+function LetterStats(props) {
+  const { letterStats } = props;
+
+  const sortedStats = letterStats.sort((a, b) => b[1] - a[1]);
+
+  return (
+    <div className='letter-stats'>
+      <h3>Letter Density</h3>
+      {sortedStats.map(([letter, count, percent]) => (
+        <div className='letter-stats-wrapper' key={letter}>
+          <div>{letter}: </div>
+          <div className='letter-stats-count'>
+            {count.toLocaleString()}
+            <span className='percent'>({percent.toFixed(2)}%)</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function WordStats(props) {
+  const { wordStats } = props;
+
+  const sortedStats = wordStats.sort((a, b) => b[1] - a[1]);
+
+  return (
+    <div className='letter-stats'>
+      <h3>Word Density</h3>
+      {sortedStats.map(([Word, count, percent]) => (
+        <div className='letter-stats-wrapper' key={Word}>
+          <div>{Word} </div>
+          <div className='letter-stats-count'>
+            {count.toLocaleString()}
+            <span className='percent'>({percent.toFixed(2)}%)</span>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
