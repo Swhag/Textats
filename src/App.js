0d763ccdc0dddf5cea12d103182be3afe3c5bdb1
Swhag <khg5293@outlook.com>
2023-02-26 19:50:35 -0500

    index.js / App.js update

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import TextAnalyzer from './components/TextAnalyzer';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return <TextAnalyzer />;
+  }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import TextAnalyzer from './components/TextAnalyzer';
+import './styles/index.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <TextAnalyzer />
+    <App />
   </React.StrictMode>
 );
